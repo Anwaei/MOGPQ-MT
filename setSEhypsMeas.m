de21271efc4f69ffc1af9cba2ef0d8a9081c7b1a
Anwaei <33192995+Anwaei@users.noreply.github.com>
2020-08-23 11:56:09 +0800

    1

--- setSEhypsMeas.m
+++ setSEhypsMeas.m
@@ -1,6 +1,6 @@
 function [l,alpha] = setSEhypsMeas(E,D)
 
-l = [80 5 0.5];  % l should be E x D
+l = [80 5 0.8];  % l should be E x D
 % l = [10 10 10];
 alpha = 1;  % alpha should be E x 1
 % alpha = 0.5;
