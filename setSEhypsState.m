8923eda99357248acf59fdc8243130c5ed097954
Anwaei <33192995+Anwaei@users.noreply.github.com>
2020-08-28 20:27:38 +0800

    1

--- setSEhypsState.m
+++ setSEhypsState.m
@@ -1,6 +1,6 @@
 function [l,alpha] = setSEhypsState(E,D)
 
-l = [80 20 0.8; 80 20 0.8; 80 20 0.8];  % l should be E x D
+l = [80 12 1; 80 12 1; 80 12 1];  % l should be E x D
 % l = [10 10 10; 10 10 10; 10 10 10];
 alpha = [1; 1; 1];  % alpha should be E x 1
 % alpha = [0.5; 0.5; 0.5];
